e35090dce50cadcd39ed22c025fa26596bb9e5d7
Kushagra Jain <jain.kushagra777@gmail.com>
2026-02-06 12:30:49 +0530

    add root route

--- app.py
+++ app.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def home():
     return "Backend is running"
 
+import os
+
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
